56b1529f17ff1bce780583b7e8b77c694901e8e0
adutilbtc <42972863+adutilbtc@users.noreply.github.com>
2018-12-21 11:44:52 -0500

    Add files via upload

--- mastermind.js
+++ mastermind.js
@@ -1,102 +1,118 @@
-/* Mastermind I - no saved turns */
+/* Class Mastermind */
 
-/* Initialization */
-//colors, code, guess, feedback -- all arrays
-var colors=['r','c','w','g','b','y'];
-var code=[];
-var guess=[];
-var feedback=[];
-/* Main Method */
-function main(){
-	code=setCode(code,colors);
+/* Initializations by Hasan*/
+var colors=[], code=[], guess=[], feedback=[];
+var turn=0;
+colors = ["r","b","g","w","c","y"];
+var thisTurn = [];
+var turnRecords = [];
 
-	guess=getGuess(guess);
-
-	feedback=testGuess(guess,code,feedback);
+function main() {
+	alert ("colors are r, c, y, w, b, g");
+	code=setCode(colors);
+	while (feedback[3]!="b" && guess[0]!="q"){
+	turn++;
+	guess=getGuess();
+	feedback=testGuess(code,guess);
+	turnRecords.push(addTurn(guess,feedback));
+	formatTurnRecords(turnRecords);
+	alert("turn and feedback "+turnRecords);	
+	alert("guess "+turn+" : "+guess+" returns: "+feedback);
+	}
+	if(feedback[3]=="b"){
+		alert("charlie you've won");
+	}
+	else if(guess[0]=="q"){
+		alert("quitter");
+	}
 }
+	
 /* Functions */
-function setCode(code,colors){
-  for(i=0; i<4; i++){
-  code[i]=colors[Math.floor(Math.random()*6)]; 
-  }
-  console.log(code);
-  return code;
+
+/* Create the Secret Code by Wyatt and Ben Woods*/
+function setCode(colors){
+	for(var i=0; i<4; i++){
+		code[i]=colors[Math.floor(Math.random()*6)];
+	}
+	console.log(code);
+	return code;
 }
 
-/* Create the Secret Code */
-
-// function pulls from six colors to randomly fill code with four values
-function getGuess(guess){
-/* Get the Player's Guess */
-  for(i=0; i<4; i++){
-guess[i]=prompt("what is your guess for position "+i);
-  }
-    console.log(guess);
-    return guess;
-  }
-// function prompts player for each of four values and stores in array
-
-/* Analyze the Guess */
-
-// function analyzes guess against code and produces feedback
-function testGuess(guess,code,feedback){
-	// initialize the temporary code array (copy of code)
-var tempCode=code;
-	// count the blacks and erase tempcode as you go - one loop
-for(g=0; g<4; g++){
-    if(guess[g]==tempCode[g] && guess[g]!=""){
-      feedback[g]="b";
-      tempCode[g]="";
-    
-  }
-}
-	// count the whites and erase tempcode as you go - nested loops
-for(g=0; g<4; g++){
-  for(c=0; c<4; c++){
-    if(guess[g]==tempCode[c] && guess[g]!=""){
-      feedback[g]="w";
-      tempCode[c]="";
-      continue;
-    }
-   }
+/* Get a Player's Guess by BenM and Chris*/
+function getGuess(){
+	for(var i=0; i<4; i++){
+		guess[i]=prompt("Enter a color for position "+(i+1));
+	}
+	//console.log(guess);
+	return guess;
 }
 
-	// use "continue" once a match is found in the inner loop
+/* Analyze the Guess by Joe and Andrew and Hasan*/
+function testGuess(code,guess){
+	var b=0, w=0;
+	var tempCode = code.slice(0);
+	var tempGuess = guess.slice(0);
+	for (var g=0;g<4;g++){
+		if (tempGuess[g]==tempCode[g]) {
+			b++;
+			tempGuess[g]="";
+			tempCode[g]="";
+		}
+	}	
+	for (var g=0; g<4;g++){
+		for (var c=0; c<4; c++){
+			if (tempGuess[g]==tempCode[c] && tempGuess[g]!=""){
+				w++;
+				tempGuess[g]="";
+				tempCode[c]="";
+				continue;
+			}
+		}
+	}
+	//console.log("Blacks = "+b+" and Whites equals "+w);
+	feedback=feedbackFormat(b,w);
+	return feedback;
+}
+function feedbackFormat(b,w){
+var feedback=[];
+for(var i=0; i<b; i++){
+	feedback[i]="b";
+}
+	for(var i=b; i<b+w;i++){
+		feedback[i]="w";
+	}
+		//console.log("feedback is "+feedback);
+		return feedback;
+}	
+function addTurn(guess, feedback){
+	var thisTurn=[];
+	var turnValues=4+feedback.length;
+	for (var i=0; i<turnValues; i++){
+		if (i < 4){
+			thisTurn[i]=guess[i];
+		}
+		else if (i > 3){
+			thisTurn[i]=feedback[i-4];
+		}
+	}
+		console.log("this turn = "+thisTurn);
+		return thisTurn;
+	
+}
+function formatTurnRecords(turnRecords){
+	var alertString = "";
+	var thisGuess = "";
+	var thisFeedback = "";
+	for (var row=0;row<turn;row++) {
+		alertString = alertString.concat("Guess "+(row+1)+" : ");
+		thisGuess=turnRecords[row].slice(0, 4).join();
+		alertString = alertString.concat(thisGuess);
+		alertString = alertString.concat(" || ");
+		
+		thisFeedback=turnRecords[row].slice(4, turnRecords[row].length).join();
+		alertString = alertString.concat("\n");
+		alert(alertString); // testing purposes
+	}
+}
 
-	// send the feeback to the formatter
-  
 
-	// console log the feedback
-console.log(feedback);
-}
-/* Format the Feedback */
-function feedbackFormat(feedback){
-// function over-writes feedback to put b's first, w's second, delete the res
-
-	// initialize the black and white count variables
-var b=0, w=0;
-  for(i=0; i<4; i++){
-	// write the black pegs
-  if(feedback[i]=="b"){
-  b++;
-}
-	// write the white pegs
-  else if (feedback[i]=="w"){
-    w++;
-  }
-  }
-	// define remainder as 4 - blacks + whites
-var remainder=4-(b+w);
-	// delete the blanks in the feedback remainder with array.pop
-for(i=0; i<b; i++){
-  feedback[i]="b";
-}
-  for(i=b; i<b+w; i++){
-  feedback[i]="w";
-}
-  for(i=0; i<remainder; i++){
-  feedback.pop();
-}
-	// return the new feedback array
-return feedback;
-}
